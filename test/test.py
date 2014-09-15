b547c3ba3e3cdf83b04b455248d0aabfdc7e2170
Janne Hakonen <joyer83@gmail.com>
2014-09-15 21:18:13 +0300

    test.py can no be ran from any folder instead of just from test/.

--- test/test.py
+++ test/test.py
@@ -1,6 +1,9 @@
-import sys
-sys.path.append("fakes")
-sys.path.append("../src/mods")
+import sys, os
+
+script_dir_path = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(script_dir_path, "fakes"))
+sys.path.append(os.path.join(script_dir_path, "../src/mods"))
+
 import BigWorld, game
 import tessu_mod
 
