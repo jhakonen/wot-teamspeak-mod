d4b8e2c5844dd534dc71a19534c7041f7422fb9c
Janne Hakonen <joyer83@gmail.com>
2014-09-20 02:43:26 +0300

    Package version to 0.2.0.

--- package.py
+++ package.py
@@ -1,12 +1,14 @@
 import py_compile, zipfile, os, fnmatch
 
+# configuration
 WOT_VERSION = "0.9.2"
-MOD_VERSION = "0.1.2"
+MOD_VERSION = "0.2.0"
 CLIENT_PACKAGE_DIR = os.path.join("res_mods", WOT_VERSION, "scripts", "client")
 BUILD_DIR = "build"
 SRC_DIR = "src"
 PACKAGE_NAME = "TessuMod-{0}.zip".format(MOD_VERSION)
 
+# compile .py files from src/ and output .pyc files to /build
 for root, dirs, files in os.walk(SRC_DIR):
 	src_dir = root
 	root2 = root[len(SRC_DIR)+1:]
@@ -19,9 +21,11 @@ for root, dirs, files in os.walk(SRC_DIR):
 		filepath = os.path.join(root, filename)
 		py_compile.compile(file=os.path.join(src_dir, filename), cfile=os.path.join(build_dir, filename)+"c", doraise=True)
 
+# remove old archive if one exists
 if os.path.exists(PACKAGE_NAME):
 	os.remove(PACKAGE_NAME)
 
+# create zip archive and compress TessuMod.txt and files from build/ to the archive
 fZip = zipfile.ZipFile(PACKAGE_NAME, "w")
 fZip.write("TessuMod.txt", "TessuMod.txt")
 for root, dirs, files in os.walk(BUILD_DIR):
