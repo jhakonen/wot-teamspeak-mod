23bbe81cf6aec8e43ddb926013f36810477d2220
Janne Hakonen <joyer83@gmail.com>
2014-09-20 02:42:57 +0300

    Added minimap notifications when player is speaking.
     - Refactored code.
     - Added comments.

--- src/mods/tessu_mod.py
+++ src/mods/tessu_mod.py
@@ -15,125 +15,93 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-import game
-from tessu_utils.ts3 import TS3Client
-from tessu_utils import utils
-from debug_utils import LOG_NOTE, LOG_ERROR, LOG_CURRENT_EXCEPTION
-import BigWorld
-import Avatar
-import Account
-import VOIP
-from gui import SystemMessages
-
 def on_talk_status_changed(user, talking):
 	'''Called when TeamSpeak user's talk status changes.
 	Parameters:
 		user: a dict with WOT nickname (empty if not available) and TeamSpeak nickname 
 		talking: True if talking, False otherwise
 	'''
-	if user["wot"]:
-		dbid = get_account_dbid_by_player_name(user["wot"])
+	player_name = (user["wot"] if user["wot"] else user["ts"]).lower()
+	g_talk_states[player_name] = talking
+	if talking:
+		# set talking state immediately
+		update_player_speak_status(player_name)
 	else:
-		dbid = get_account_dbid_by_player_name(user["ts"])
-	if dbid is not None:
-		g_talk_states[dbid] = talking
-		if talking:
-			# set talking state immediately
-			update_player_speak_status(dbid)
-		else:
-			# keep talking state for a little longer
-			BigWorld.callback(1, utils.with_args(update_player_speak_status, dbid))
+		# keep talking state for a little longer
+		BigWorld.callback(1, utils.with_args(update_player_speak_status, player_name))
 
-def get_account_dbid_by_player_name(player_name):
+def update_player_speak_status(player_name):
+	'''Updates given 'player_name's talking status to VOIP system and minimap.''' 
 	player_name = player_name.lower()
-	dbid = None
-	if get_my_name().lower() == player_name:
-		dbid = get_my_dbid()
-	if dbid:
-		return dbid
-	vehicle = get_vehicle_by_player_name(player_name)
-	if vehicle:
-		return vehicle["accountDBID"]
-	info = get_prebattle_account_info_by_player_name(player_name)
-	if info:
-		return info["dbID"]
-
-def get_my_name():
+	info = utils.get_player_info_by_name(player_name)
 	try:
-		return BigWorld.player().name
-	except AttributeError:
+		VOIP.getVOIPManager().setPlayerTalking(info["dbid"], g_talk_states[player_name])
+	except:
 		pass
-
-def get_my_dbid():
 	try:
-		return BigWorld.player().databaseID
-	except AttributeError:
+		if g_talk_states[player_name]:
+			g_marker_repeater.start(info["vehicle_id"])
+		else:
+			g_marker_repeater.stop(info["vehicle_id"])
+	except:
 		pass
-
-def get_vehicle_by_player_name(player_name):
-	'''Searches vehicle which has given 'player_name' and returns it.
-	Returns None if matching vehicle was not found.
-	'''
-	try:
-		for vehicle in BigWorld.player().arena.vehicles.values():
-			if player_name == vehicle["name"].lower():
-				return vehicle
-	except AttributeError:
-		pass
-
-def get_prebattle_account_info_by_player_name(player_name):
-	try:
-		rosters = BigWorld.player().prebattle.rosters
-		for roster in rosters:
-			for id in rosters[roster]:
-				info = rosters[roster][id]
-				if player_name == info["name"].lower():
-					return info
-	except AttributeError:
-		pass
-
-def update_player_speak_status(dbid):
-	VOIP.getVOIPManager().setPlayerTalking(dbid, g_talk_states[dbid])
 
 def on_connected_to_ts3():
-	push_system_message("Connected to TeamSpeak client", SystemMessages.SM_TYPE.Warning)
+	'''Called when TessuMod manages to connect TeamSpeak client. However, this
+	doesn't mean that the client is connected to any TeamSpeak server.
+	'''
+	utils.push_system_message("Connected to TeamSpeak client", SystemMessages.SM_TYPE.Warning)
 
 def on_disconnected_from_ts3():
-	push_system_message("Disconnected from TeamSpeak client", SystemMessages.SM_TYPE.Warning)
-
-def push_system_message(message, type):
-    try:
-        if SystemMessages.g_instance is None:
-            BigWorld.callback(1, utils.with_args(push_system_message, message, type))
-        else:
-            SystemMessages.pushMessage(message, type)
-    except:
-        LOG_CURRENT_EXCEPTION()
-        return
+	'''Called when TessuMod loses connection to TeamSpeak client.'''
+	g_marker_repeater.stop_all()
+	utils.push_system_message("Disconnected from TeamSpeak client", SystemMessages.SM_TYPE.Warning)
 
 def Player_onBecomePlayer(orig_method):
 	def wrapper(self):
+		'''Called when BigWorld.player() is available.'''
 		orig_method(self)
 		# save my WOT nickname to TeamSpeak's meta data so that other TeamSpeak
 		# users can match the name to vehicles in battle when I'm speaking
 		g_ts.set_wot_nickname(BigWorld.player().name)
+
 	return wrapper
 
 def VOIPManager_isParticipantTalking(orig_method):
 	def wrapper(self, dbid):
-		if dbid in g_talk_states:
-			return g_talk_states[dbid]
+		'''Called by other game modules to determine current speaking status.'''
+		try:
+			return g_talk_states[utils.get_player_info_by_dbid(dbid)["player_name"].lower()]
+		except:
+			pass
 		return orig_method(self, dbid)
 	return wrapper
 
-Avatar.Avatar.onBecomePlayer = Player_onBecomePlayer(Avatar.Avatar.onBecomePlayer)
-Account.PlayerAccount.onBecomePlayer = Player_onBecomePlayer(Account.PlayerAccount.onBecomePlayer)
-VOIP.VOIPManager.isParticipantTalking = VOIPManager_isParticipantTalking(VOIP.VOIPManager.isParticipantTalking)
+try:
+	import game
+	from tessu_utils.ts3 import TS3Client
+	from tessu_utils import utils
+	from debug_utils import LOG_NOTE, LOG_ERROR, LOG_CURRENT_EXCEPTION
+	import BigWorld
+	import Avatar
+	import Account
+	import VOIP
+	from gui import SystemMessages
 
-g_talk_states = {}
-g_ts = TS3Client()
-g_ts.connect()
-g_ts.on_connected += on_connected_to_ts3
-g_ts.on_disconnected += on_disconnected_from_ts3
-g_ts.on_talk_status_changed += on_talk_status_changed
-utils.call_in_loop(0.1, g_ts.check_events)
+	# do all intializations here
+	g_talk_states = {}
+	g_marker_repeater = utils.MarkerRepeater(interval=3.5, action="attackSender")
+	g_ts = TS3Client()
+	g_ts.connect()
+	g_ts.on_connected += on_connected_to_ts3
+	g_ts.on_disconnected += on_disconnected_from_ts3
+	g_ts.on_talk_status_changed += on_talk_status_changed
+	utils.call_in_loop(0.1, g_ts.check_events)
+
+	# if nothing broke so far then it should be safe to patch the needed
+	# functions (modified functions have dependencies to g_* global variables)
+	Avatar.Avatar.onBecomePlayer = Player_onBecomePlayer(Avatar.Avatar.onBecomePlayer)
+	Account.PlayerAccount.onBecomePlayer = Player_onBecomePlayer(Account.PlayerAccount.onBecomePlayer)
+	VOIP.VOIPManager.isParticipantTalking = VOIPManager_isParticipantTalking(VOIP.VOIPManager.isParticipantTalking)
+except:
+	LOG_CURRENT_EXCEPTION()
--- src/mods/tessu_utils/utils.py
+++ src/mods/tessu_utils/utils.py
@@ -18,7 +18,9 @@
 import BigWorld
 import threading
 from debug_utils import LOG_CURRENT_EXCEPTION
-
+from gui import SystemMessages
+from gui.WindowsManager import g_windowsManager
+	
 def call_in_loop(secs, func):
 	def wrapper(*args, **kwargs):
 		func(*args, **kwargs)
@@ -73,3 +75,151 @@ class ThreadCall(object):
 		except Exception as e:
 			self._error = e
 		self._finished.set()
+
+def find_vehicle_id(matcher):
+	'''Finds 'vehicle_id' using given 'matcher' function.'''
+	try:
+		vehicles = BigWorld.player().arena.vehicles
+		for id in vehicles:
+			vehicle = vehicles[id]
+			if matcher(vehicle):
+				return id
+	except AttributeError:
+		pass
+	return None
+
+def get_vehicle(vehicle_id):
+	'''Returns vehicle info with matching 'vehicle_id' if available.
+	Returns None if not.
+	'''
+	if vehicle_id is None:
+		return None
+	try:
+		return BigWorld.player().arena.vehicles[vehicle_id]
+	except AttributeError:
+		pass
+
+def get_my_name():
+	'''Returns current player's nickname. None if not available.'''
+	try:
+		return BigWorld.player().name
+	except AttributeError:
+		pass
+
+def get_my_dbid():
+	'''Returns current player's account dbid. None if not available.'''
+	try:
+		return BigWorld.player().databaseID
+	except AttributeError:
+		pass
+
+def find_prebattle_account_info(player_name, matcher):
+	'''Finds player information from prebattle rosters (e.g. in practise room
+	when you assign players to teams). Given 'matcher' function is used pick
+	desired info. Returns None if nothing found.
+	'''
+	try:
+		rosters = BigWorld.player().prebattle.rosters
+		for roster in rosters:
+			for id in rosters[roster]:
+				info = rosters[roster][id]
+				if matcher(info):
+					return info
+	except AttributeError:
+		pass
+
+def get_player_info_by_name(player_name):
+	'''Extracts player information with matching 'player_name' from
+	various locations. Returns 'dbid' and 'vehicle_id' in a dict if available,
+	returns empty dict if nothing found.
+	''' 
+	player_name = player_name.lower()
+	vehicle_id = find_vehicle_id(lambda v: v["name"].lower() == player_name)
+	if vehicle_id is not None:
+		return {
+			"dbid": get_vehicle(vehicle_id)["accountDBID"],
+			"vehicle_id": vehicle_id
+		}
+	if get_my_name().lower() == player_name:
+		return {
+			"dbid": get_my_dbid()
+		}
+	info = find_prebattle_account_info(lambda i: i["name"].lower() == player_name)
+	if info:
+		return {
+			"dbid": info["dbID"]
+		}
+	return {}
+
+def get_player_info_by_dbid(dbid):
+	'''Extracts player information with matching account 'dbid' from
+	various locations. Returns 'player_name' and 'vehicle_id' in a dict if
+	available, returns empty dict if nothing found.
+	''' 
+	vehicle_id = find_vehicle_id(lambda v: v["accountDBID"] == dbid)
+	if vehicle_id is not None:
+		return {
+			"player_name": get_vehicle(vehicle_id)["name"],
+			"vehicle_id": vehicle_id
+		}
+	if get_my_dbid() == dbid:
+		return {
+			"player_name": get_my_name()
+		}
+	info = find_prebattle_account_info(lambda i: i["dbID"] == dbid)
+	if info:
+		return {
+			"player_name": info["name"]
+		}
+	return {}
+
+def push_system_message(message, type):
+	'''Pushes given 'message' to system notification center in garage. 'type'
+	is one of the SystemMessages.SM_TYPE.* constants.
+	'''
+	try:
+		if SystemMessages.g_instance is None:
+			BigWorld.callback(1, with_args(push_system_message, message, type))
+		else:
+			SystemMessages.pushMessage(message, type)
+	except:
+		LOG_CURRENT_EXCEPTION()
+		return
+
+import time
+
+class MarkerRepeater(object):
+	'''MarkerRepeater class repeatably starts given marker 'action' every
+	'interval' seconds in minimap over given 'vehicle_id', effectively creating
+	continuous animation until the marker action is stopped.
+	'''
+
+	def __init__(self, interval, action):
+		self._interval = interval
+		self._action = action
+		self._running_ids = set()
+
+	def start(self, vehicle_id):
+		'''Starts playing action marker for given 'vehicle_id'.'''
+		if vehicle_id not in self._running_ids:
+			self._running_ids.add(vehicle_id)
+			self._repeat(vehicle_id)
+
+	def stop(self, vehicle_id):
+		'''Stops playing action marker for given 'vehicle_id'.'''
+		try:
+			self._running_ids.remove(vehicle_id)
+		except KeyError:
+			pass
+
+	def stop_all(self):
+		'''Stops all marker animations.'''
+		self._running_ids.clear()
+
+	def _repeat(self, vehicle_id):
+		try:
+			if vehicle_id in self._running_ids:
+				g_windowsManager.battleWindow.minimap.showActionMarker(vehicle_id, self._action)
+				BigWorld.callback(self._interval, with_args(self._repeat, vehicle_id))
+		except AttributeError:
+			pass
