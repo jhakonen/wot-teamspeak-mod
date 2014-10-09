eabbff3e5ba094e14b0e7cfa6a1bcb8b573c70d8
Janne Hakonen <joyer83@gmail.com>
2014-10-09 21:52:09 +0300

    Added more notification test features.

--- features/notifications.feature
+++ features/notifications.feature
@@ -8,3 +8,16 @@ Feature: Notifications in garage
 		When user starts and logins to WOT
 		Then "Connected to TeamSpeak client" is shown in notification center
 		And no errors occurred
+
+	Scenario: User starts TS while WOT is running
+		Given WOT is running and in garage
+		When user starts TS
+		Then "Connected to TeamSpeak client" is shown in notification center
+		And no errors occurred
+
+	Scenario: TS and WOT is running and TS is closed
+		Given TS is running
+		And WOT is running and in garage
+		When user closes TS
+		Then "Disconnected from TeamSpeak client" is shown in notification center
+		And no errors occurred
--- features/steps/notifications.py
+++ features/steps/notifications.py
@@ -1,20 +1,29 @@
 from behave import *
+import time
+
+@when("user starts TS")
+def step_impl(context):
+	time.sleep(1)
+	context.ts_client.start()
+	set_ts_client_not_connected_to_server_responses(context.ts_client)
 
 @given("TS is running")
 def step_impl(context):
 	context.ts_client.start()
-	context.ts_client.set_cmd_response(
-		"clientnotifyregister schandlerid=0 event=notifytalkstatuschange")
-	context.ts_client.set_cmd_response(
-		"clientnotifyregister schandlerid=0 event=notifycliententerview")
-	context.ts_client.set_cmd_response(
-		"clientnotifyregister schandlerid=0 event=notifyclientupdated")
-	context.ts_client.set_cmd_response(
-		"clientnotifyregister schandlerid=0 event=notifyclientuidfromclid")
-	context.ts_client.set_cmd_response("whoami", error=(1794, "not connected"))
+	set_ts_client_not_connected_to_server_responses(context.ts_client)
+
+@when("user closes TS")
+def step_impl(context):
+	context.ts_client.stop()
+
+@given("WOT is running and in garage")
+def step_impl(context):
+	context.game.start()
+	context.game.login()
 
 @when("user starts and logins to WOT")
 def step_impl(context):
+	time.sleep(1)
 	context.game.start()
 	context.game.login()
 
@@ -27,3 +36,14 @@ def step_impl(context):
 	for log in context.game.get_logs():
 		assert log[0] != "ERROR", "Error in log output: {0}".format(log)
 		assert log[0] != "EXCEPTION", "Exception in log output: {0}".format(log)
+
+def set_ts_client_not_connected_to_server_responses(ts_client):
+	ts_client.set_cmd_response(
+		"clientnotifyregister schandlerid=0 event=notifytalkstatuschange")
+	ts_client.set_cmd_response(
+		"clientnotifyregister schandlerid=0 event=notifycliententerview")
+	ts_client.set_cmd_response(
+		"clientnotifyregister schandlerid=0 event=notifyclientupdated")
+	ts_client.set_cmd_response(
+		"clientnotifyregister schandlerid=0 event=notifyclientuidfromclid")
+	ts_client.set_cmd_response("whoami", error=(1794, "not connected"))
