ef2728fcc1aa05cdfe8c36cd6c35a7ecc1734ae5
Janne Hakonen <joyer83@gmail.com>
2014-10-30 21:37:46 +0200

    Refactoring: Split UserCache class to smaller classes.

--- src/mods/tessu_utils/user_cache.py
+++ src/mods/tessu_utils/user_cache.py
@@ -27,36 +27,19 @@ import Event
 class UserCache(object):
 
 	def __init__(self, ini_path):
-		self.on_reloaded = Event.Event()
-		self._sync_time = 0
-		self._parser = None
-		self._check_interval = 5
-		self._write_needed = False
 		self._ts_users = {}
 		self._players = {}
 		self._pairings = {}
-		self._ini_path = ini_path
+		self._ini_cache = INICache(ini_path)
+		self._ini_cache.on_read += self._on_read
+		self._ini_cache.on_write += self._on_write
+		self._ini_cache.init()
 
-		self._read_cache_file()
-		self._write_cache_file()
-		self._sync()
-
-	def _read_cache_file(self):
-		if not os.path.isfile(self._ini_path):
-			return
-		parser = self._create_parser()
-		if not parser.read(self._ini_path):
-			LOG_ERROR("Failed to parse ini file '{0}'"
-				.format(self._ini_path))
-			return
-
+	def _on_read(self, parser):
 		pairings = {}
-		for player_nick, ts_nicks in parser.items("PlayerUserPairings"):
+		for (player_nick,) in parser.items("PlayerUserPairings"):
 			player_id = parser.getint("GamePlayers", player_nick)
-			pairings[player_id] = []
-			for ts_nick in csv._from_csv_string(ts_nicks):
-				ts_id = parser.getint("TeamSpeakUsers", ts_nick)
-				pairings[player_id].append(ts_id)
+			pairings[player_id] = [parser.getint("TeamSpeakUsers", ts_nick) for ts_nick in parser.getlist("PlayerUserPairings", player_nick)]
 		ts_users = {id: nick for nick, id in parser.items("TeamSpeakUsers")}
 		players = {id: nick for nick, id in parser.items("GamePlayers")}
 
@@ -64,20 +47,10 @@ class UserCache(object):
 		self._players = players
 		self._pairings = pairings
 
-		self._update_sync_time()
-
-	def _update_sync_time(self):
-		self._sync_time = self._get_modified_time()
-
-	def _create_parser(self):
-		parser = ConfigParser.SafeConfigParser()
+	def _on_write(self, parser):
 		parser.add_section("TeamSpeakUsers")
 		parser.add_section("GamePlayers")
 		parser.add_section("PlayerUserPairings")
-		return parser		
-
-	def _write_cache_file(self):
-		parser = self._create_parser()
 		for id in self._ts_users:
 			parser.set("TeamSpeakUsers", self._ts_users[id], str(id))
 		for id in self._players:
@@ -85,21 +58,71 @@ class UserCache(object):
 		for player_id in self._pairings:
 			player_nick = self._players[player_id]
 			ts_nicks = [self._ts_users[ts_id] for ts_id in self._pairings[player_id]]
-			parser.set("PlayerUserPairings", player_nick, self._to_csv_string(ts_nicks))
+			parser.set("PlayerUserPairings", player_nick, ts_nicks)
 
+	def set_ini_check_interval(self, interval):
+		self._ini_cache.ini_check_interval = interval
+
+	def add_ts_user(self, user):
+		LOG_NOTE("TS user: {0} ({1})".format(user.nick, user.unique_id))
+		if user.unique_id not in self._ts_users:
+			self._ts_users[user.unique_id] = user.nick
+		self._ini_cache.write_needed()
+
+	def add_player(self, player):
+		LOG_NOTE("Player: {0} ({1})".format(player.getName(), player.getID()))
+		if player.getID() not in self._players:
+			self._players[player.getID()] = player.getName()
+		self._ini_cache.write_needed()
+
+class INICache(object):
+
+	def __init__(self, ini_path):
+		self._sync_time = 0
+		self._parser = None
+		self._check_interval = 5
+		self._write_needed = False
+		self._ini_path = ini_path
+		self.on_read = Event.Event()
+		self.on_write = Event.Event()
+
+	def init(self):
+		self._read_cache_file()
+		self._write_cache_file()
+		self._sync()
+
+	@property
+	def check_interval(self):
+		return self._check_interval
+
+	@check_interval.setter
+	def ini_check_interval(self, interval):
+		self._check_interval = interval
+
+	def write_needed(self):
+		self._write_needed = True
+
+	def _read_cache_file(self):
+		if not os.path.isfile(self._ini_path):
+			return
+		parser = ExtendedConfigParser()
+		if not parser.read(self._ini_path):
+			LOG_ERROR("Failed to parse ini file '{0}'"
+				.format(self._ini_path))
+			return
+		self.on_read(parser)
+		self._update_sync_time()
+
+	def _update_sync_time(self):
+		self._sync_time = self._get_modified_time()
+
+	def _write_cache_file(self):
+		parser = ExtendedConfigParser()
+		self.on_write(parser)
 		with open(self._ini_path, "w") as f:
 			parser.write(f)
 		self._update_sync_time()
 		self._write_needed = False
-
-	def _to_csv_string(self, values):
-		string_io = io.StringIO()
-		csv_out = csv.writer(string_io)
-		csv_out.writerow(values)
-		return string_io.getvalue().rstrip("\r\n")
-
-	def _from_csv_string(self, csv_string):
-		return csv.reader([csv_string])[0]
 
 	def _sync_cache_file(self):
 		if self._is_cache_file_modified():
@@ -118,22 +141,17 @@ class UserCache(object):
 
 	def _sync(self):
 		self._sync_cache_file()
-		BigWorld.callback(self.get_ini_check_interval(), self._sync)
+		BigWorld.callback(self.ini_check_interval, self._sync)
 
-	def get_ini_check_interval(self):
-		return self._check_interval
+class ExtendedConfigParser(ConfigParser.SafeConfigParser):
 
-	def set_ini_check_interval(self, interval):
-		self._check_interval = interval
+	def getlist(self, section, option):
+		return csv.reader([self.get(section, option)])[0]
 
-	def add_ts_user(self, user):
-		LOG_NOTE("TS user: {0} ({1})".format(user.nick, user.unique_id))
-		if user.unique_id not in self._ts_users:
-			self._ts_users[user.unique_id] = user.nick
-		self._write_needed = True
-
-	def add_player(self, player):
-		LOG_NOTE("Player: {0} ({1})".format(player.getName(), player.getID()))
-		if player.getID() not in self._players:
-			self._players[player.getID()] = player.getName()
-		self._write_needed = True
+	def set(self, section, option, value):
+		if isinstance(value, list) or isinstance(value, tuple):
+			string_io = io.StringIO()
+			csv_out = csv.writer(string_io)
+			csv_out.writerow(value)
+			value = string_io.getvalue().rstrip("\r\n")
+		ConfigParser.SafeConfigParser.set(self, section, option, value)
