1ac766f452c3ac62e936774e87c6788b30b4a56b
Janne Hakonen <joyer83@gmail.com>
2015-03-27 06:54:43 +0200

    Changed mod version to 0.6.0-test.

--- package.py
+++ package.py
@@ -20,7 +20,7 @@ import subprocess
 
 # configuration
 WOT_VERSION = "0.9.6"
-MOD_VERSION = "0.5.1"
+MOD_VERSION = "0.6.0-test"
 CLIENT_PACKAGE_DIR = os.path.join("res_mods", WOT_VERSION, "scripts", "client")
 BUILD_DIR = os.path.join(os.getcwd(), "build")
 SRC_DIR = os.path.join(os.getcwd(), "src")
