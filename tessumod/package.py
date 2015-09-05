d3fd1483a6fc313bc2767405e79a7dd6180b38b4
Janne Hakonen <joyer83@gmail.com>
2015-09-05 21:52:47 +0300

    Updated WoT version to 0.9.10.

--- tessumod/package.py
+++ tessumod/package.py
@@ -7,7 +7,7 @@ import shutil
 import argparse
 import re
 
-WOT_VERSION          = "0.9.9"
+WOT_VERSION          = "0.9.10"
 SUPPORT_URL          = "http://forum.worldoftanks.eu/index.php?/topic/433614-/"
 ROOT_DIR             = os.path.dirname(os.path.realpath(__file__))
 SRC_DIR              = os.path.join(ROOT_DIR, "src")
