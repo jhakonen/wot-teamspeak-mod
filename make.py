1e9de6c53dd82a42eee2863d4dd9b2c0a5db3393
Janne Hakonen <joyer83@gmail.com>
2016-03-20 10:25:43 +0200

    Fixed bytecode generation.

--- make.py
+++ make.py
@@ -20,7 +20,8 @@ import os
 import json
 
 # prevent *.pyc-file generation
-os.environ["PYTHONDONTWRITEBYTECODE"] = "1"
+sys.dont_write_bytecode = True # for current process
+os.environ["PYTHONDONTWRITEBYTECODE"] = "1" # for subprocesses
 
 from invoke import ctask as task, Program, Argument, Config, Collection
 from termcolor import colored
