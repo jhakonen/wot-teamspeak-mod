6edd924ad0d9b93eea146179f0504cfd750c6211
Janne Hakonen <janne.hakonen@iki.fi>
2019-07-12 08:09:44 +0300

    Fixed fute suite's event loop not properly cancelling callback calls.

--- test/fute/test_helpers/event_loop.py
+++ test/fute/test_helpers/event_loop.py
@@ -4,51 +4,62 @@ import time
 class EventLoop(object):
 
 	def __init__(self):
-		self.__callbacks = []
+		self.__once_callbacks = []
+		self.__repeat_callbacks = []
 
 	def fini(self):
-		del self.__callbacks[:]
+		del self.__once_callbacks[:]
+		del self.__repeat_callbacks[:]
 
 	def execute(self):
 		self.__exit_called = False
 		while not self.__exit_called:
-			for callback in self.__callbacks:
+			for callback in list(self.__once_callbacks):
 				if callback.should_call():
-					self.__callbacks.remove(callback)
-					callback()
-					if callback.repeat:
-						self.__callbacks.append(callback)
-
+					self.__once_callbacks.remove(callback)
+					callback.call()
+			for callback in list(self.__repeat_callbacks):
+				if callback.should_call():
+					callback.call()
+					callback.reset()
 			time.sleep(0.001)
 
 	def exit(self):
 		self.__exit_called = True
 
 	def call(self, callback, repeat=False, timeout=0):
-		self.__callbacks.append(Callback(callback, repeat, timeout))
+		if repeat:
+			self.__repeat_callbacks.append(Callback(callback, timeout))
+		else:
+			self.__once_callbacks.append(Callback(callback, timeout))
 
 	def cancel_call(self, callback):
-		for callback_obj in self.__callbacks:
+		found = False
+		for callback_obj in self.__once_callbacks:
 			if callback_obj.callback == callback:
-				callback_obj.repeat = False
-				self.__callbacks.remove(callback_obj)
+				self.__once_callbacks.remove(callback_obj)
+				found = True
 				break
+		if not found:
+			for callback_obj in self.__repeat_callbacks:
+				if callback_obj.callback == callback:
+					self.__repeat_callbacks.remove(callback_obj)
+					found = True
+					break
+		assert found, "No such callback: %s" % callback
 
 class Callback(object):
 
-	def __init__(self, callback, repeat, timeout):
+	def __init__(self, callback, timeout):
 		self.callback = callback
-		self.repeat = repeat
 		self.timeout = timeout
-		self.__set_time_end()
+		self.reset()
 
-	def __set_time_end(self):
+	def reset(self):
 		self.time_end = time.time() + self.timeout
 
 	def should_call(self):
 		return time.time() >= self.time_end
 
-	def __call__(self):
+	def call(self):
 		self.callback()
-		if self.repeat:
-			self.__set_time_end()
